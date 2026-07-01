2ef347cddae83740908b72cae43ef5f238eecdd9
Mayur Nimbalkar <mayurnimbalkar013@gmail.com>
2026-07-01 16:48:41 +0530

    updated Navbar

--- Leave Management system/Frontend/src/app/features/navbar/navbar.ts
+++ Leave Management system/Frontend/src/app/features/navbar/navbar.ts
@@ -1,10 +1,58 @@
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/authService';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
 @Component({
   selector: 'app-navbar',
-  imports: [RouterOutlet, MatToolbarModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar  {
+  isAuthenticated = false;
+  currentUser: any = null;
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+  ) {}
+
+  ngOnInit(): void {
+
+    this.syncAuthState();
+    this.authService.currentUser$.subscribe(() => {
+      this.syncAuthState();
+    });
+  }
+
+  private syncAuthState(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+    this.currentUser = this.authService.getCurrentUser();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.syncAuthState();
+    this.router.navigate(['/login']);
+  }
+
+  isHOD(): boolean {
+    return this.authService.userRole() === 'hod';
+  }
+}
+
