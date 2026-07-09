3e1dc501ac5e3f2f20e6482f7cd567ec33413b77
Mayur Nimbalkar <mayurnimbalkar013@gmail.com>
2026-07-09 16:04:24 +0530

    Middlewares updated

--- backend/middlewares/authGuard.js
+++ backend/middlewares/authGuard.js
@@ -26,11 +26,9 @@ export const authGuard = (req, res, next) => {
 export const roleGuard = (roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return res
-        .status(403)
-        .json({
-          message: "Access denied. You dont have the required permissions.",
-        });
+      return res.status(403).json({
+        message: "Access denied. You dont have the required permissions.",
+      });
     }
     next();
   };
