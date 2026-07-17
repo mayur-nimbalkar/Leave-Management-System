682e065b7bec4f0af195591fd983cc5200d7f77c
Mayur Nimbalkar <mayurnimbalkar013@gmail.com>
2026-07-17 22:27:35 +0530

    fixed wrong database URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   const mongoUri =
-    process.env.MONGO_URI || "mongodb://127.0.0.1:27017/leave-management";
+    process.env.MONGO_URI || "mongodb://127.0.0.1:27017/LeaveManagementSystem";
 
   try {
     await mongoose.connect(mongoUri);
