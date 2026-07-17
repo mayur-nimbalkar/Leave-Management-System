b9b41c5fe2b2fa3a78d1d432cc35c9f54136c76e
Mayur Nimbalkar <mayurnimbalkar013@gmail.com>
2026-07-17 22:29:03 +0530

    added route for balance

--- backend/routes/leaveRoutes.js
+++ backend/routes/leaveRoutes.js
@@ -6,6 +6,7 @@ import {
 } from "../controllers/leaveController.js";
 import { authGuard, roleGuard } from "../middlewares/authGuard.js";
 import Leave from "../models/Leave.js";
+import LeaveBalance from "../models/LeaveBalance.js";
 import { validateLeaveInputs } from "../middlewares/leaveMiddleware.js";
 
 const router = express.Router();
@@ -25,4 +26,30 @@ router.get(
   roleGuard(["hod", "staff"]),
   getLeaveRecordsServiceController,
 );
-export default router;
+
+router.get("/balance", authGuard, async (req, res) => {
+  try {
+    const balance = await LeaveBalance.findOne({ employeeId: req.user.userId });
+    if (!balance) {
+      return res.status(404).json({
+        success: false,
+        message: "Leave balance not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      data: {
+        CL: balance.CL,
+        SL: balance.SL,
+        EL: balance.EL,
+        CompOff: balance.compOff,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching balance:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Error fetching leave balance",
+    });
+  }
+});
